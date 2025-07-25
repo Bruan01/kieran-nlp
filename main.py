39c68d30110860376404ec3f255a3d14261e3eaf
[bruan] <[1441584221@qq.com]>
2025-07-26 02:06:41 +0800

    私有化api为环境变量

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget
 from chat_interface import ChatCore
 from chat_widget import ChatWidget
+from dotenv import load_dotenv
+import os
+# 加载.env
+load_dotenv()
 
 # ===============对话主窗口====================
 class NLPDesktopApp(QMainWindow):
@@ -11,22 +15,20 @@ class NLPDesktopApp(QMainWindow):
         super().__init__()
         self.setWindowTitle("NLP 工作台")
         self.resize(900, 600)
-
         self.tabs = QTabWidget()
-        # 配置你的API KEY和URL
-
-        chat_core = ChatCore(api_key="sk-wopmheqzfejiqbtefflzawoalyqaqiaoqbsmmfpspiekyrcx", 
-                             api_url="https://api.siliconflow.cn/v1/chat/completions")
+        # 从环境变量读取.env文件
+        api_key = os.getenv("API_KEY")
+        api_url = os.getenv("API_URL")
+        chat_core = ChatCore(api_key=api_key,api_url=api_url)
         
         self.rag_tab = ChatWidget(chat_core)
         self.model_tab = QWidget()
         self.api_tab = QWidget()
-
         self.tabs.addTab(self.rag_tab, "RAG 问答")
         self.tabs.addTab(self.model_tab, "模型推理")
         self.tabs.addTab(self.api_tab, "API 控制台")
-
         self.setCentralWidget(self.tabs)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
